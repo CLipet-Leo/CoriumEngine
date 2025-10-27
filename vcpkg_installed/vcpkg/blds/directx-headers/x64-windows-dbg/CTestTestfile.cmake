f0dcbe9b3bd5631ad6ea7d8c6f411aa4552b4d24
Léo <leoclipet26@gmail.com>
2025-10-27 23:44:10 +0100

    Updated dependencies and excluded Intermediate

---
+++ vcpkg_installed/vcpkg/blds/directx-headers/x64-windows-dbg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Leo/Documents/Code projects/CoriumEngine/vcpkg_installed/vcpkg/blds/directx-headers/src/v1.618.2-0d044a4cd1.clean
+# Build directory: C:/Users/Leo/Documents/Code projects/CoriumEngine/vcpkg_installed/vcpkg/blds/directx-headers/x64-windows-dbg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
